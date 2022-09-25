d3103b12b8829b24341282b3b4f48cddb5969f47
dm1itri <dimma-18@yandex.ru>
2022-09-25 23:02:03 +0300

    шаблоны с циклами и условиями

--- profile/views.py
+++ profile/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'profile/index.html')
+    city = 'Санкт-Петербург'
+    hobbies = ['Футбол', 'Программирование в совокупности с прослушиванием музыки',
+               'Велосипедные прогулки', 'Осуществление мыслительных процессов, лежа на кровати:)']
+    experience = [(None, 'Голосовой помощник для ведения дневника с домашним заданием, к сожалению, не сохранен в облаке (PyQt5, speech_recognition)'),
+                  ('https://github.com/dm1itri/checkers', 'Реализация шашек (PyGame, socket)'),
+                  ('https://github.com/dm1itri/WEB_PROJECT', 'Создания сайта для определения направления в крупной сфере IT (Flask)'),
+                  (None, 'Создание простых чат-ботов в Telegram, VK'),
+                  ('https://github.com/dm1itri/individual-project-10',
+                   'Сейчас разрабатываю веб-игру для представления, как Итоговый проект в школе (в данным момент реализуется фронтенд: HTML5, SASS, JS)')]
+    return render(request, 'profile/index.html', {'city': city, 'hobbies': hobbies, 'experience': experience})
